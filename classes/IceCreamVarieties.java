ee276ce9f794ade185eade20223cb88b849eb62a
Pathumi Rashmika <sakuni.pathumi@gmail.com>
2022-10-11 14:45:03 +0530

    Add files via upload

---
+++ classes/IceCreamVarieties.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author PATHUMI
+ */
+public abstract class IceCreamVarieties {
+
+    public String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract Double iceCreamPrice();
+    
+    
+    
+}
+
+
